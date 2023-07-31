3d767dd79d3ebd269862aaf91a8ce5cb1036b575
Aman chaudhary <123990844+theamanchaudhary@users.noreply.github.com>
2023-07-31 15:08:31 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -25,4 +25,12 @@ async def search(bot, message):
                name = (msg.text or msg.caption).split("\n")[0]
                if name in results:
                   continue 
-               results += f"<b><I>♻️ {name}\n🔗 {msg.link}</I></b>\n\n"                                                      
+               results += f"<b><I>♻️ {name}\n🔗 {msg.link}</I></b>\n\n"
+        if bool(results)==False:
+          movies = await (query)
+          buttons = []
+          for movie in movies: 
+              buttons.append([InlineKeyboardButton(movie['title'], callback_data=f"recheck_{movie['id']}")])
+          msg = await message.reply_photo(photo="https://telegra.ph/file/997e3df68001ca73a845f.jpg",
+                                          caption="<b><I>I Couldn't find anything related to Your Query😕.\nDid you mean any of these?</I></b>", 
+                                          reply_markup=InlineKeyboardMarkup(buttons))
