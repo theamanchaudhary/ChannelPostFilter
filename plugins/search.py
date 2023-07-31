3e5618b51961149081a1976f02b1fbf240b22cde
Aman chaudhary <123990844+theamanchaudhary@users.noreply.github.com>
2023-07-31 15:04:28 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -26,24 +26,3 @@ async def search(bot, message):
                if name in results:
                   continue 
                results += f"<b><I>♻️ {name}\n🔗 {msg.link}</I></b>\n\n"                                                      
-       
-
-
-@Client.on_callback_query(filters.regex(r"^request"))
-async def request(bot, update):
-    clicked = update.from_user.id
-    try:      
-       typed = update.message.reply_to_message.from_user.id
-    except:
-       return await update.message.delete()       
-    if clicked != typed:
-       return await update.answer("That's not for you! 👀", show_alert=True)
-
-    admin = (await get_group(update.message.chat.id))["user_id"]
-    id    = update.data.split("_")[1]
-    name  = await search_imdb(id)
-    url   = "https://www.imdb.com/title/tt"+id
-    text  = f"#RequestFromYourGroup\n\nName: {name}\nIMDb: {url}"
-    await bot.send_message(chat_id=admin, text=text, disable_web_page_preview=True)
-    await update.answer("✅ Request Sent To Admin", show_alert=True)
-    await update.message.delete(60)
