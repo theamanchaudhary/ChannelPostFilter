e6aab1f7e9ced7e02cc381fa9bee775a398969c6
Aman chaudhary <123990844+theamanchaudhary@users.noreply.github.com>
2025-02-02 19:56:11 +0530

    2

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 21746097
-API_HASH     = "2e17eb9d1b6b55bcc156fcb1d476f46a"
-BOT_TOKEN    = "6192240484:AAHuhg33vVhT2IiHXIaTPLEEQsuP-j2SmL0"
-SESSION      = "BQG_0ykAZSX0YzsWpgNqIpwwjYBGR5hYgQC2VlDI9B5IqOU55KVKpNFCmX_uWj6tKowgPh7Iy5eurLBM5YkdARsngieAaLLvBSLdueBYe0RtUF7dr-F1sYfELJ6nKkj3W9XjdOiQnlnlnIq5ch1rBmjcN9DmjU71c3Sbhn6kuvoGRl5dwdIcFvhSWp1D_ePEOdWIrhke-C0RtT8lmt_35LadI2_vzigotELx5QRNCj4n7jS_3ioEkMWK408CfPFm5_RYzOw_CxZYSuJvcPs6tJSoHqdMakheIiWibhRSz2nhc9TSzC3vts9AmJL9ZrPzxbfLSp5dj4qGWxEP8p1v8nWg56TmNgAAAAFCHsc1AA"
-DATABASE_URI = "mongodb+srv://theamanchaudhary:updatesbyaman@cluster0.ykowsgs.mongodb.net/?retryWrites=true&w=majority"
-LOG_CHANNEL  = -1001910513374
-ADMIN        = 5674333293
-CHANNEL      = "@UpdatesByAman"
+API_ID       = ""
+API_HASH     = ""
+BOT_TOKEN    = ""
+SESSION      = ""
+DATABASE_URI = ""
+LOG_CHANNEL  = 
+ADMIN        = 
+CHANNEL      = ""
